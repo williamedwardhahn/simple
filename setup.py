dd123ca757d99e1986f91a3ccdeff175d941bce0
William E Hahn <williamedwardhahn@gmail.com>
2020-10-19 20:30:35 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='simple',
+      version='0.1',
+      description='Simple turtle net code',
+      url='https://github.com/williamedwardhahn/simple',
+      author='W Hahn',
+      author_email='williamedwardhahn@gmail.com',
+      license='MIT',
+      packages=['simple'],
+      zip_safe=False)
